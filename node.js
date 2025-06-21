60890d2720060732779cdc256de0078872e54a82
freemyswagfms <anisacoy7@gmail.com>
2025-06-21 23:16:12 +0300

    Add files via upload

--- node.js
+++ node.js
@@ -9,10 +9,15 @@ const bot = new Telegraf(process.env.BOT_TOKEN);
 
 // Пример простой команды
 bot.command('start', (ctx) => {
-  ctx.reply('Привет! Это тестовый бот для мини-приложения. Кнопка для запуска mini app появится скоро!');
+  ctx.reply('Открыть приложение:', {
+    reply_markup: {
+      inline_keyboard: [[
+        {
+          text: 'Открыть мини-приложение',
+          web_app: { url: 'https://flip-fmsbot.vercel.app/' }
+        }
+      ]]
+    }
+  });
 });
 
-// Запуск бота
-bot.launch();
-
-console.log('Бот запущен!');
